cdcd8bc4616909977849526e0c7663fdb27ea765
Carleigh Crockett <capleugh@gmail.com>
2019-10-16 10:06:10 -0600

    Add highest_paid_actor method and test

--- lib/network.rb
+++ lib/network.rb
@@ -9,4 +9,14 @@ class Network
   def add_show(show)
     @shows.push(show)
   end
+
+  def highest_paid_actor
+    characters = @shows.map do |show|
+      show.characters
+    end.flatten
+
+    characters.max_by do |char|
+      char.salary
+    end.actor
+  end
 end
--- test/network_test.rb
+++ test/network_test.rb
@@ -30,4 +30,11 @@ class NetworkTest < Minitest::Test
 
     assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
   end
+
+  def test_it_can_find_highest_paid_actor
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+    
+    assert_equal "Amy Poehler", @nbc.highest_paid_actor
+  end
 end
